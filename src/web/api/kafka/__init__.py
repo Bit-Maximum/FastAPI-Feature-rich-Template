b19420f29fea6eca12165eb22aea04b04ef32b05
Bit Maximum <cabamax@yandex.ru>
2025-08-08 20:30:09 +1000

    Initialise project via fastapi_template

---
+++ src/web/api/kafka/__init__.py
@@ -0,0 +1,5 @@
+"""API to interact with kafka."""
+
+from src.web.api.kafka.views import router
+
+__all__ = ["router"]
---
+++ src/web/api/redis/__init__.py
@@ -0,0 +1,5 @@
+"""Redis API."""
+
+from src.web.api.redis.views import router
+
+__all__ = ["router"]
